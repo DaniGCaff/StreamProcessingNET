b7acea739220a06f57ae6690eec6c2835dd9c921
Dani Cafferata <danigcaff@gmail.com>
2017-09-11 19:54:06 +0200

    Add files via upload
    Initial commit: basic point-to-point queue

---
+++ StreamProcessingNET/StreamProcessingNET/IUpdateDashboardService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace StreamProcessingNET
+{
+    [ServiceContract]
+    public interface IUpdateDashboardService
+    {
+
+        [OperationContract(IsOneWay=true)]
+        void UpdateDashboardByGraphID(int value);
+    }
+}
